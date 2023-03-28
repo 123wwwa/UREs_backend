4302171ea2a3310ce61f34dc25535e3906d8b51b
123wwwa <123wwa@naver.com>
2023-03-28 13:54:57 +0900

    Update mariadb.config.ts

--- mariadb.config.ts
+++ mariadb.config.ts
@@ -3,7 +3,7 @@ import mariadb from 'mariadb';
 const pool = mariadb.createPool({
     host: '127.0.0.1',
     user: 'test',
-    password: 'aboorh1237',
+    password: 'test',
     connectionLimit: 5,
 })
 export default pool;
